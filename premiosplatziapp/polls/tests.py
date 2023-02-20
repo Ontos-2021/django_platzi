9a24e42933fe3e83bd8f86dda061be4b834d49ee
Ontos <josemiguel.mercado17@gmail.com>
2023-02-20 19:18:39 -0300

    Testing Views

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.test import TestCase
+from django.urls.base import reverse
 from django.utils import timezone
 
 from .models import Question
@@ -12,3 +13,11 @@ class QuestionModelTest(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(question_text = "¿Quién es el mejor Course Director de Platzi?", pub_date = time)
         self.assertIs(future_question.was_published_recently(), False)
+
+class QuestionIndexViewTest(TestCase):
+
+    def test_no_questions(self):
+        """If no question exists, an appropiate message is displayed"""
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        
