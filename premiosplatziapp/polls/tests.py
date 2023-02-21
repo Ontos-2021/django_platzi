b0d4deaad01e7d9a862b45af5b533dce72b0f356
Ontos <josemiguel.mercado17@gmail.com>
2023-02-21 14:23:52 -0300

    Agregue un test para dos preguntas futuras

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -68,3 +68,13 @@ class QuestionIndexViewTest(TestCase):
         past_question2 = create_question(question_text="Past question 2", days=-40)
         response = self.client.get(reverse("polls:index"))
         self.assertQuerysetEqual(response.context["latest_question_list"], [past_question1, past_question2])
+
+    def test_two_future_questions(self):
+        '''
+        If exist just two questions in the future in the database, no questions are displayed
+        '''
+        future_question1 = create_question(question_text="Future question 1", days=30)
+        future_question2 = create_question(question_text="Future question 2", days=40)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
+        
